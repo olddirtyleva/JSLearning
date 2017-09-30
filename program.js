89d43c987b1e1362a0e2a03f39199702dadd3163
jskonst <jskonst@yandex.ru>
2017-09-30 15:21:02 +0300

    first lab

--- program.js
+++ program.js
@@ -1,9 +1,27 @@
 console.log("Hello world");
-var a = 5;
-console.log(a);
-console.log(a==5);
-console.log(a=="5");
-console.log(a==="5");
-let c = "Some";
-c = c.toUpperCase();
-console.log(c);
+let a = 'sdf';
+console.log("value a=%f some %f other",5,6);
+if (a==5)
+    console.log("value a=%f some other",5);
+    console.log("a="+6);
+
+switch (a){
+    case 'sdf':{
+        console.log("Nope");
+        break;
+    }
+    case 'a':{
+        console.log("Nope1");
+        break;
+    }
+}
+var arr=[5,6,7,8,9];
+for (var i in arr){
+//     console.log(i)
+    console.log(arr[i]);
+}
+let j=0;
+while( j <10){
+ console.log(j);
+j++;
+}
