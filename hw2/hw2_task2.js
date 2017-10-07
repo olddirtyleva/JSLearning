5799f6cd5de0942684c9021183d7923ec197cc05
Lev Zonin <Levzonin@mail.ru>
2017-10-07 10:01:59 +0300

    second task

---
+++ hw2/hw2_task2.js
@@ -0,0 +1,11 @@
+var n;
+function makeArray() {
+    var items = [];
+    for (var i = 0; i <= 10; i++) {
+    items.push(i);
+    }
+    return items;
+    }
+    var army = makerray();
+    alert (army[0]);
+    alert (army[5]);
